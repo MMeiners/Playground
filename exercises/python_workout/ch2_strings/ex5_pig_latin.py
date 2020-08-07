c12f2be7d67c517cf3b65fcab2cb9af77e58efff
Mark Meiners <50118209+MMeiners@users.noreply.github.com>
2020-08-07 16:59:47 -0700

    First run at completing ex5

---
+++ exercises/python_workout/ch2_strings/ex5_pig_latin.py
@@ -0,0 +1,28 @@
+# Problem: write a function that takes a string as an english word and applies a pig latin transformation
+# Input: string
+# Output: pig latin translated form of input string
+
+def pig_latin(word):
+    """Transform string using pig latin rules"""
+
+    try:
+        word = str(word)
+
+        if not word.isalpha():
+            raise ValueError
+
+        if word[0] in 'aeiou':
+            transformed_word = f'{word}way'
+        else:
+            transformed_word = f'{word[1:]}{word[0]}ay'
+
+        return transformed_word
+    except (TypeError, ValueError):
+        return 'Not a valid word'
+
+
+print(pig_latin('apple'))
+print(pig_latin('python'))
+print(pig_latin('apple123'))
+print(pig_latin('123'))
+print(pig_latin(['a']))
