572061208f79671c2bf8e0e51b350c8a55d87080
Mark Meiners <50118209+MMeiners@users.noreply.github.com>
2020-08-24 18:12:11 -0700

    -Started ex24 but need to walk away for a bit, committing and will finish in a bit.

---
+++ exercises/python_workout/ch5_files/ex24_reverse_lines.py
@@ -0,0 +1,12 @@
+# Problem: transform strings from one file and write to a second.  Transform is a basic reverse
+# Input: two input filename strings
+# Output: reversed lines written to disk
+
+import pathlib
+
+
+def reverse_lines(read_file: str, write_file: str) -> None:
+    """ Reverse the lines found in the read_file and write them out """
+    pass
+
+
