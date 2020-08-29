7ae1d838765892b2723a3ffccf85c7bc27d9b284
Mark Meiners <50118209+MMeiners@users.noreply.github.com>
2020-08-29 14:37:48 -0700

    -First try at ex31

---
+++ exercises/python_workout/ch7_comprehensions/ex31_pig_latin_file.py
@@ -0,0 +1,23 @@
+# Problem: translate a file into pig latin and return the new string
+# Input: a text file and file location
+# Output: translated string value
+
+import ex5_pig_latin
+pig = ex5_pig_latin.pig_latin
+
+
+def make_pig_latin(filename: str) -> str:
+    """ Translate the file into pig latin """
+
+    translation = ''
+
+    with open(filename) as file:
+
+        translator = (pig(word) for line in file for word in line.split() if pig(word) != 'Not a valid word')
+
+        translation = ' '.join(translator)
+
+    return translation
+
+
+print(make_pig_latin('..\\ch5_files\\wcfile.txt'))
