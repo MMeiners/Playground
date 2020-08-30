f239285c508562256b2e9ac896e6d5798d2fc378
Mark Meiners <50118209+MMeiners@users.noreply.github.com>
2020-08-30 15:48:33 -0700

    -first try at ex32

--- exercises/python_workout/ch7_comprehensions/ex32_flip_a_dict.py
+++ exercises/python_workout/ch7_comprehensions/ex32_flip_a_dict.py
@@ -3,3 +3,16 @@
 # Input: dict
 # Output: dict with the key value pairs role reversed.
 
+
+def flip_dict(input_dict: dict) -> dict:
+    """ Flip the key:value pairs around """
+
+    return {value: key for key, value in input_dict.items()}
+
+
+users = {'Luke': 1,
+         'Leia': 2,
+         'Han': 3,
+         'Yoda': 4}
+
+print(flip_dict(users))
