46431d62c739f0378af10cfe7f1e4a0aff923891
Mark Meiners <50118209+MMeiners@users.noreply.github.com>
2020-09-03 17:40:15 -0700

    -modified ex39 to solve ex40 which meant adding a class level attribute max_scoops and checking against it in add_scoop -did rename a variable just a little for readability

--- exercises/python_workout/ch9_objects/ex39_ice_cream_bowl.py
+++ exercises/python_workout/ch9_objects/ex39_ice_cream_bowl.py
@@ -6,6 +6,8 @@ from ch9_objects.ex38_ice_cream_scoop import Scoop
 
 
 class Bowl:
+    MAX_SCOOPS = 3
+
     def __init__(self):
         self.contents = []
 
@@ -15,19 +17,22 @@ class Bowl:
     def add_scoop(self, *scoops: Scoop):
         """ Add a scoop to the bowl """
 
-        for scoop in scoops:
-            self.contents.append(scoop)
+        for a_scoop in scoops:
+            if len(self.contents) < Bowl.MAX_SCOOPS:
+                self.contents.append(a_scoop)
 
 
 if __name__ == '__main__':
     scoop1 = Scoop('Chocolate')
     scoop2 = Scoop('Coffee')
     scoop3 = Scoop('Rum Raisin')
+    scoop4 = Scoop('Butter Pecan')
 
     my_bowl = Bowl()
 
     my_bowl.add_scoop(scoop1, scoop2)
     my_bowl.add_scoop(scoop3)
+    my_bowl.add_scoop(scoop4)
 
     print('My bowl has...')
     print(my_bowl)
