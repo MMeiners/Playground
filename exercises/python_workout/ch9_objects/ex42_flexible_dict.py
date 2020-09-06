e3bfe181f1d78d2ae8297a7ed83a3657e0de8de2
Mark Meiners <50118209+MMeiners@users.noreply.github.com>
2020-09-05 21:29:49 -0700

    -ex42 done

--- exercises/python_workout/ch9_objects/ex42_flexible_dict.py
+++ exercises/python_workout/ch9_objects/ex42_flexible_dict.py
@@ -4,17 +4,38 @@
 # Output: value object found in the dict
 
 class FlexibleDict(dict):
-    pass
+    """ A more flexible dict index """
+
+    def __getitem__(self, item):
+        """ override normal [] access to a dict trying to cast the key to match """
+
+        key = item
+
+        try:
+            if key in self:
+                pass
+            elif int(key) in self:
+                key = int(key)
+            elif str(key) in self:
+                key = str(key)
+            else:
+                raise ValueError
+
+            return super().__getitem__(key)
+        except (ValueError, TypeError):
+            return 'nothing here!'
 
 
 if __name__ == '__main__':
     test_dict = FlexibleDict()
-    test_dict[1] = 'one int'
-    test_dict['2'] = 'two str'
-    test_dict[3] = 'three int'
-    test_dict['3'] = 'three str'
+    test_dict[1] = 'int one'
+    test_dict['1'] = 'str one'
+    test_dict['2'] = 'str two'
+    test_dict[3] = 'int three'
 
     print(f'Search int 1, found {test_dict[1]}')
     print(f'Search str 1, found {test_dict["1"]}')
     print(f'Search int 2, found {test_dict[2]}')
-    print(f'Search int 3, found {test_dict[3]}')
+    print(f'Search str 3, found {test_dict["3"]}')
+    print(f'Search str a, found {test_dict["a"]}')
+    print(f'Search int 20, found {test_dict[20]}')
