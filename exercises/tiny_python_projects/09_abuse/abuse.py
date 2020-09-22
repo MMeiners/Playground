c47aafd8d96bcb2a01c75948bf548c43dc4f6126
Mark Meiners <50118209+MMeiners@users.noreply.github.com>
2020-09-22 16:43:18 -0700

    -09 tests pass, code done

--- exercises/tiny_python_projects/09_abuse/abuse.py
+++ exercises/tiny_python_projects/09_abuse/abuse.py
@@ -5,6 +5,19 @@ Purpose: Random insults
 """
 
 import argparse
+import random
+
+ADJECTIVES = """bankrupt base caterwauling corrupt cullionly 
+detestable dishonest false filthsome filthy foolish foul gross heedless 
+indistinguishable infected insatiate irksome lascivious lecherous 
+loathsome lubbery old peevish rascaly rotten ruinous scurilous scurvy 
+slanderous sodden-witted thin-faced toad-spotted unmannered vile 
+wall-eyed""".split()
+
+NOUNS = """Judas Satan ape ass barbermonger beggar block boy braggart butt 
+carbuncle coward coxcomb cur dandy degenerate fiend fishmonger fool gull 
+harpy jack jolthead knave liar lunatic maw milksop minion ratcatcher 
+recreant rogue scold slave swine traitor varlet villain worm""".split()
 
 
 # --------------------------------------------------
@@ -37,7 +50,15 @@ def get_args():
                         type=int,
                         default=None)
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if args.number < 1:
+        parser.error(f'--number "{args.number}" must be > 0')
+
+    if args.adjectives < 1:
+        parser.error(f'--adjectives "{args.adjectives}" must be > 0')
+
+    return args
 
 
 # --------------------------------------------------
@@ -45,6 +66,14 @@ def main():
     """ Start here """
 
     args = get_args()
+    random.seed(args.seed)
+    number_adjectives = args.adjectives
+    number_insults = args.number
+
+    for _ in range(number_insults):
+        colorful_adjectives = random.sample(ADJECTIVES, number_adjectives)
+        insulting_name = random.choice(NOUNS)
+        print(f'You {", ".join(colorful_adjectives)} {insulting_name}!')
 
 
 # --------------------------------------------------
