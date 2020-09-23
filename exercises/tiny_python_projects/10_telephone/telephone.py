7d830de123bf001aa9d7e7e94083452c4646201d
Mark Meiners <50118209+MMeiners@users.noreply.github.com>
2020-09-23 14:42:28 -0700

    -tests pass, code done... -worth noting... to pass the tests, not only do you need the random seed set, you need to make sure the list of replacement values is sorted the same way as the author and contains the same choice values which in this case included replacing values each time through the loop.  That caught me while trying to code this.

--- exercises/tiny_python_projects/10_telephone/telephone.py
+++ exercises/tiny_python_projects/10_telephone/telephone.py
@@ -6,6 +6,8 @@ Purpose: You said, but I heard...
 
 import argparse
 import random
+import os
+import string
 
 
 # --------------------------------------------------
@@ -35,7 +37,16 @@ def get_args():
                         type=float,
                         default=.1)
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if args.mutations < 0 or args.mutations > 1:
+        parser.error(f'--mutations "{args.mutations}" must be between 0 and 1')
+
+    if os.path.isfile(args.text):
+        with open(args.text, encoding='utf-8') as file:
+            args.text = file.read()
+
+    return args
 
 
 # --------------------------------------------------
@@ -43,6 +54,21 @@ def main():
     """ Start here """
 
     args = get_args()
+    text = args.text.rstrip()
+    scrambled = text
+    random.seed(args.seed)
+    length = len(text)
+
+    num_mutations = round(length * args.mutations)
+    new_chars = ''.join(sorted(string.ascii_letters + string.punctuation))
+    indexes_to_swap = random.sample(range(length), num_mutations)
+
+    for index in indexes_to_swap:
+        new_char = random.choice(new_chars.replace(scrambled[index], ''))
+        scrambled = scrambled[:index] + new_char + scrambled[index+1:]
+
+    print(f'You said: "{text}"')
+    print(f'I heard : "{scrambled}"')
 
 
 # --------------------------------------------------
