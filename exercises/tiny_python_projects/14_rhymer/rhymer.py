096617f09d5bac81114ace61451f919335325a94
Mark Meiners <50118209+MMeiners@users.noreply.github.com>
2020-09-27 15:31:16 -0700

    -test and template for project 14

---
+++ exercises/tiny_python_projects/14_rhymer/rhymer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Date   : 2020-09-27
+Purpose: Let's rhyme
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    # noinspection PyTypeChecker
+    parser = argparse.ArgumentParser(
+        description='Make rhyming "words"',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('word',
+                        metavar='word',
+                        help='A word to rhyme')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """ Start here """
+
+    args = get_args()
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
